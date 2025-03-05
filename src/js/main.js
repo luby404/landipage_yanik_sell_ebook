2751bd7065347f539906743028061310b2ea4008
Ricardo Cayoca <116099669+luby404@users.noreply.github.com>
2025-03-05 17:12:05 +0100

    index.html, app.js
    adicinar as funcionalidades de temporizador e melhorar o tempo de alerta de vendas

--- src/js/main.js
+++ src/js/main.js
@@ -17,25 +17,25 @@ var slideUp = {
     opacity: 0
 };
 
-ScrollReveal({ reset: true}).reveal('.infos', slideUp);
+ScrollReveal({ reset: true }).reveal('.infos', slideUp);
 ScrollReveal({ reset: true, delay: 300 }).reveal('.card_img', slideUp);
 
 
 
 
 document.querySelectorAll(".cards .card").forEach((element, index) => {
-    ScrollReveal({ reset: true, delay: index*200 }).reveal(element, slideUp);
+    ScrollReveal({ reset: true, delay: index * 200 }).reveal(element, slideUp);
 });
 
 document.querySelectorAll(".cards .test_mu").forEach((element, index) => {
-    ScrollReveal({ reset: true, delay: index*200 }).reveal(element, slideUp);
+    ScrollReveal({ reset: true, delay: index * 200 }).reveal(element, slideUp);
 });
 
 
 let msg = document.querySelector(".message")
 
 setInterval(() => {
-    
+
 
     msg.classList.add("show")
     msg.innerHTML = `
@@ -47,12 +47,65 @@ setInterval(() => {
 
     setTimeout(() => {
         msg.classList.remove("show")
-    }, 1500);
-    
-
-}, 5000);
+    }, 2100);
 
 
+}, 15000);
 
 
+
+let timer;
+let isRunning = false;
+let milliseconds = 0, seconds = 0, minutes = 10;
+
+function startTimer() {
+    if (!isRunning) {
+        isRunning = true;
+        timer = setInterval(updateTime, 10);
+    }
+}
+
+function pauseTimer() {
+    isRunning = false;
+    clearInterval(timer);
+}
+
+function resetTimer() {
+    isRunning = false;
+    clearInterval(timer);
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 10;
+    document.getElementById("display").innerText = "10:00:00";
+}
+
+function updateTime() {
+    if (milliseconds === 0 && seconds === 0 && minutes === 0) {
+        clearInterval(timer);
+        isRunning = false;
+        return;
+    }
+
+    milliseconds -= 10;
+    if (milliseconds < 0) {
+        milliseconds = 990;
+        seconds--;
+    }
+    if (seconds < 0) {
+        seconds = 59;
+        minutes--;
+    }
+
+    let min = minutes < 10 ? "0" + minutes : minutes;
+    let sec = seconds < 10 ? "0" + seconds : seconds;
+    //let ms = milliseconds < 100 ? "0" + milliseconds : milliseconds;
+    document.getElementById("timer").innerText = `${min}:${sec}`;
+
+
+
+}
+
+setTimeout(() => {
+    startTimer()
+}, 1000);
 
